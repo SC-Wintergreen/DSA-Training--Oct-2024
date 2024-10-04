6fccb8275ab9ef8895e49741477bd02f3af9c307
SC-GLOC <official.sagnikchakraborty@outlook.com>
2024-10-04 07:35:10 +0530

    DSA Training Day 1

---
+++ Arrays And ArrayLists/Day 1/StarPattern.java
@@ -0,0 +1,25 @@
+
+/*
+ *    
+ *   * * * *
+ *   * * * *
+ *   * * * *
+ *   * * * *
+ */
+
+public class StarPattern {
+
+    public static void main(String[] args) {
+
+        int input = 5;
+        for (int i = 0; i < input; i++) {
+            for (int j = 0; j < input; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+}
---
+++ Arrays And ArrayLists/Day 1/StarPattern1.java
@@ -0,0 +1,29 @@
+/*
+ * 
+ *   *
+ *   * *
+ *   * * *
+ *   * * * *
+ * 
+ * 
+ */
+
+public class StarPattern1 {
+
+    public static void main(String[] args) {
+
+        int input = 4;
+        for (int i = 1; i <= input; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                System.out.print("* ");
+
+            }
+
+            System.out.println();
+        }
+
+    }
+
+}
